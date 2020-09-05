471a295e6a8239dcf179ce79e9869df4f30edcd4
jrbtaylor <jrbtaylor@users.noreply.github.com>
2020-09-04 20:29:17 -0400

    add work in progress on IIR prefilter

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import scipy.io.wavfile
 LEN_CLIP = 10
 LEN_SILENCE = 0.25
 OUTPUT_LEVELS = 40
-RATE = 22050
+RATE = 44100
 
 
 def save_wav(data, filename, rate=RATE):
@@ -44,13 +44,14 @@ def find_latency(train_signal='chirp'):
     def normalize(x):
         x = np.sign(x).astype('float')
         return x
+
     train_label = normalize(train_label)
     train_signal = normalize(train_signal)
 
-    max_shift = int(RATE*3*1e-3)
+    max_shift = int(RATE * 3 * 1e-3)
     shifts = np.arange(0, max_shift)
-    mses = [np.mean(np.square(train_signal-train_label)) if shift == 0
-            else np.mean(np.square(train_signal[shift:]-train_label[:-shift]))
+    mses = [np.mean(np.square(train_signal - train_label)) if shift == 0
+            else np.mean(np.square(train_signal[shift:] - train_label[:-shift]))
             for shift in shifts]
     print(mses)
     print(np.mean(np.square(train_label)))
@@ -62,15 +63,15 @@ def generate_noise(length, std=0.1):
 
 
 def generate_chirp(length, f_start, f_end, harmonic_ratio):
-    n_harmonics = int(np.floor(np.log2(RATE/2/max([f_start, f_end]))))
-    if max([f_start, f_end])*2**n_harmonics >= RATE/2:
+    n_harmonics = int(np.floor(np.log2(RATE / 2 / max([f_start, f_end]))))
+    if max([f_start, f_end]) * 2 ** n_harmonics >= RATE / 2:
         raise ValueError('Cannot have %i harmonics. Highest frequency is above the Nyquist rate' % n_harmonics)
-    t = np.linspace(0, length, int(length*RATE))
-    f = np.linspace(f_start, f_end, int(length*RATE), endpoint=True)
-    signal = np.zeros(int(length*RATE), dtype='float')
+    t = np.linspace(0, length, int(length * RATE))
+    f = np.linspace(f_start, f_end, int(length * RATE), endpoint=True)
+    signal = np.zeros(int(length * RATE), dtype='float')
     for h in range(n_harmonics):
-        fh = f*2**h
-        signal += (harmonic_ratio**h)*np.sin(2*np.pi*fh*t)
+        fh = f * 2 ** h
+        signal += (harmonic_ratio ** h) * np.sin(2 * np.pi * fh * t)
     return signal
 
 
@@ -81,11 +82,11 @@ def generate_train_signal(saveto):
     :param saveto: path to save training wav file
     :return:
     """
-    length = int((LEN_CLIP-LEN_SILENCE)*RATE)
+    length = int((LEN_CLIP - LEN_SILENCE) * RATE)
     signal = []
     for std in np.logspace(-3.5, -1, OUTPUT_LEVELS, endpoint=True):
         signal.append(generate_noise(length, std=std))
-        signal.append(np.zeros([int(RATE*LEN_SILENCE)]))
+        signal.append(np.zeros([int(RATE * LEN_SILENCE)]))
     signal = np.concatenate(signal)
     save_wav(signal, saveto, RATE)
 
@@ -93,23 +94,37 @@ def generate_train_signal(saveto):
 def generate_train_signal2(saveto):
     signal = []
     f_low = 40  # bass low E
-    f_high = 80*8  # twelfth fret high E
-    for harmonic_ratio in [0.1*i for i in range(10)]:
+    f_high = 80 * 8  # twelfth fret high E
+    for harmonic_ratio in [0.1 * i for i in range(10)]:
         chirp = generate_chirp(LEN_CLIP - LEN_SILENCE, f_low, f_high, harmonic_ratio)
         for i, std in enumerate(np.logspace(-3.5, -0.6, OUTPUT_LEVELS, endpoint=True)):
-            x = np.sqrt(2)*std*chirp
+            x = np.sqrt(2) * std * chirp
             signal.append(x)
-            signal.append(np.zeros([int(RATE*LEN_SILENCE)]))
+            signal.append(np.zeros([int(RATE * LEN_SILENCE)]))
     signal = np.concatenate(signal)
     save_wav(signal, saveto, RATE)
 
 
+def prefilter(wav_file, saveto, n_filters=40, f_min=40, f_max=16000, fs=44100):
+    from filterbank import filterbank
+    from scipy.signal import lfilter
+
+    signal, rate = load_wav(wav_file)
+    assert rate == fs
+
+    filters = filterbank(n_filters, f_min, f_max, fs)
+    for idx, (b, a) in enumerate(filters):
+        x = lfilter(b, a, signal)
+        save_wav(x, saveto + '_%i' % idx + '.wav', fs)
+
+
 def split_signal(data):
     # split_length = 96000  # this appears to be the limit for laptop cpu training
-    split_length = int(np.ceil(data.size/(data.size//1e5)))
+    # split_length = int(np.ceil(data.size/(data.size//1e5)))  # limit for gpu training with FIR filter
+    split_length = int(np.ceil(data.size / (data.size // 8e4)))  # Try this for IIR filter?
     if data.size % split_length != 0:
-        data = np.concatenate([data, np.zeros(split_length-(data.size % split_length), dtype=data.dtype)], axis=0)
-    data = np.array(np.split(data, np.ceil(data.size/split_length)))
+        data = np.concatenate([data, np.zeros(split_length - (data.size % split_length), dtype=data.dtype)], axis=0)
+    data = np.array(np.split(data, np.ceil(data.size / split_length)))
     data = np.expand_dims(data, axis=1)
     return data
     # data = np.expand_dims(np.expand_dims(data, axis=0), axis=0)
@@ -118,4 +133,4 @@ def split_signal(data):
 
 def clear_print(string='', end=''):
     clear = '\x1b[2K\r'
-    print(clear+string, end=end)
+    print(clear + string, end=end)
